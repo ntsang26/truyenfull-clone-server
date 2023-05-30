f9397196c48bfee77ed2a6c8b77233c3d9878bb6
bogdev26 <sangnt@jitsinnovationlabs.com>
2023-05-30 23:04:54 +0700

    update

--- controllers/StoryController.js
+++ controllers/StoryController.js
@@ -1,45 +1,45 @@
-const Story = require('../model/Story')
+const Story = require("../model/Story")
 const { v4: uuidv4 } = require("uuid")
 
 const StoryController = {
-  find: async (req, res) => {
-    try {
-      let { queryInput, sort } = req.body
-      let stories = await Story.find(queryInput).sort(sort)
-      if (!stories.length) return res.json({ errorCode: 1, errorMsg: "No data" })
-      return res.json({
-        errorCode: 0,
-        errorMsg: "Success",
-        data: stories
-      });
-    } catch (error) {
-      console.log("StoryController", error)
-      res.error()
-    }
-  },
-  create: async (req, res) => {
-    try {
-      if (Object.keys(req.body).length <= 0) {
-        return res.json({
-          errorCode: 1,
-          errorMsg: "Failed",
-        })
-      }
-
-      let opt = {
-        ...req.body,
-        sid: uuidv4()
-      }
-      await Story.create(opt)
-      return res.json({
-        errorCode: 0,
-        errorMsg: "Success",
-      })
-    } catch (error) {
-      console.log("StoryController", error)
-      res.error()
-    }
-  }
+	find: async (req, res) => {
+		try {
+			let { queryInput = {}, sort = { createdAt: 1 } } = req.body
+			let stories = await Story.find(queryInput).sort(sort).limit(10)
+			if (!stories.length)
+				return res.json({ errorCode: 1, errorMsg: "No data" })
+			return res.json({
+				errorCode: 0,
+				errorMsg: "Success",
+				data: stories,
+			})
+		} catch (error) {
+			console.log("StoryController", error)
+			res.error()
+		}
+	},
+	create: async (req, res) => {
+		try {
+			if (Object.keys(req.body).length <= 0) {
+				return res.json({
+					errorCode: 1,
+					errorMsg: "Failed",
+				})
+			}
+			let opt = {
+				...req.body,
+				sid: uuidv4(),
+			}
+			await Story.create(opt)
+			return res.json({
+				errorCode: 0,
+				errorMsg: "Success",
+			})
+		} catch (error) {
+			console.log("StoryController", error)
+			res.error()
+		}
+	},
 }
 
 module.exports = StoryController
